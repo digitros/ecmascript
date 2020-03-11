1cc0efae8975d73926fb59491618d4c19ce5f0b0
Diego Valdez Acosta <dgogo9@hotmail.com>
2020-03-11 17:42:40 -0600

    Clases, modulos y generadores

--- src/es6/index.js
+++ src/es6/index.js
@@ -131,3 +131,63 @@ helloPromise()
 
 
 ////
+
+
+class calculator{
+    constructor(){
+        this.valueA = 0;
+        this.valueB = 0;
+    }
+    sum(valueA, valueB){
+        this.valueA = valueA;
+        this.valueB = valueB;
+        return this.valueA + this.valueB;
+    }
+}
+
+const calc = new calculator();
+console.log(calc.sum(2, 2));
+
+////
+
+const hello = require('./module');
+
+console.log(hello());
+
+////
+
+function* helloWorld(){
+    if (true){
+        yield 'Hello, ';
+    }
+    if(true){
+        yield 'World';
+    }
+};
+
+const generatorHello = helloWorld();
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
+
+////
+
+function* fibonacci(){
+    let fn1 = 1;
+    let fn2 = 1;
+    while(true){
+        let actual = fn2;
+        fn2 = fn1;
+        fn1 = fn1 + actual;
+        let reset = yield actual;
+        if(reset){
+            fn1 = 1;
+            fn2 = 2;
+        }
+    }
+}
+
+let secuencia = fibonacci();
+for (i = 1; i <= 9; i++){
+    console.log(secuencia.next().value)
+}
