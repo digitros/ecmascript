1de5474e585614e4f396bef2b3eb611dc05277b5
Diego Valdez Acosta <dgogo9@hotmail.com>
2020-03-11 13:00:26 -0600

    Let, Const, multilinea, spread operator y desestructuración

--- src/es6/index.js
+++ src/es6/index.js
@@ -1,17 +1,11 @@
+////
+
 function newFunction(name, age, country){
     var name = name || 'Diego';
     var age = age || 23;
     var country = country || 'MX';
     console.log(name, age, country);
 }
-
-let hello = 'Hello';
-let world = 'World';
-let epicPhrase = hello + ' ' + world;
-console.log(epicPhrase);
-
-
-// es6
 
 function newFunction2(name = 'Diego', age = 23, country = 'MX'){
     console.log(name, age, country);
@@ -20,6 +14,66 @@ function newFunction2(name = 'Diego', age = 23, country = 'MX'){
 newFunction2();
 newFunction2('Sharon', 23, 'MX')
 
+////
+
+let hello = 'Hello';
+let world = 'World';
+let epicPhrase = hello + ' ' + world;
+console.log(epicPhrase);
+
 
 let epicPhrase2 = `${hello} ${world}`
 console.log(epicPhrase2);
+
+////
+
+let lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit \n"
++ "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+
+let lorem2 = `Lorem ipsum dolor sit amet, consectetur adipiscing elit
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`;
+
+console.log(lorem);
+console.log(lorem2);
+
+////
+
+let person = {
+    'name': 'Diego',
+    'age': 23,
+    'country': 'MX'
+}
+
+console.log(person.name, person.age);
+
+let{ name, age, country } = person;
+console.log(name, age, country)
+
+////
+
+let team1 = ['Oscar', 'Julian', 'Ricardo'];
+let team2 = ['Valeria', 'Yesica', 'Camila'];
+
+let education = ['Diego', ...team1, ...team2];
+
+console.log(education);
+
+////
+
+{
+    var globalVar = "Global Var";
+}
+
+{
+    let globalLet = 'Global Let';
+    console.log(globalLet);
+}
+
+console.log(globalVar);
+
+////
+
+const a = 'b';
+console.log(a);
+
+////
