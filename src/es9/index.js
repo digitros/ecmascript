9c905f3384492922d3692fb50cc3a19d405eb781
Diego Valdez Acosta <dgogo9@hotmail.com>
2020-03-12 23:41:48 -0600

    es9

---
+++ src/es9/index.js
@@ -0,0 +1,48 @@
+const obj = {
+    name: 'Diego',
+    age: 23,
+    country: 'MX',
+};
+
+let { country, ...all} = obj;
+console.log(all)
+
+////
+
+const obj = {
+    name: 'Diego',
+    age: 23,
+};
+
+const obj1 = {
+    ...obj,
+    country: 'MX',
+};
+
+console.log(obj1);
+
+////
+
+const helloWorld = () => {
+    return new Promise((resolve, reject) => {
+        (true)
+            ? setTimeout(() => resolve('Hello World'), 3000)
+            : reject(new Error('Test Error'))
+    });
+};
+
+helloWorld()
+    .then(response => console.log(response))
+    .catch(error => console.log(error))
+    .finally(() => console.log('Finalizó'))
+
+
+////
+
+const rejexData = /([0-9]{4})-([0-9]{2})-([0-9]{2})/
+const match = rejexData.exec('2018-04-20');
+const year = match[1]
+const month = match[2]
+const day = match[3]
+
+console.log(`${year}-${month}-${day}`);
